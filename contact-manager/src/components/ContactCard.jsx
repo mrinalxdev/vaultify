97fc349dd3f9b448676ae4cfc93aec321011481a
Hi_Mrinal <mrinalbitsat@gmail.com>
2023-03-10 11:23:22 +0530

    Custome URL for every Contact

--- contact-manager/src/components/ContactCard.jsx
+++ contact-manager/src/components/ContactCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import user from '../images/user.png'
+import {Link} from 'react-router-dom'
 
 const ContactCard = (props) => {
 
@@ -9,8 +10,10 @@ const ContactCard = (props) => {
     <div className="item" style={{display:"flex", justifyContent:"space-between"}}>
       <div className="content">
       <img className='ui avatar image' src={user} alt="" />
+      <Link to={`/contact/${id}`}>
         <div className="header">{name}</div>
         <div>{email}</div>
+      </Link>
       </div>
       <i className="trash alternate outline icon" style={{color:'red', marginTop:"7px"}}  onClick={() => props.clickHandler(id)}></i>
     </div>
