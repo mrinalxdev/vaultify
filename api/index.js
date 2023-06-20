61c7bb877e256b5029ab499b0de56bd123c88cab
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-06-21 02:42:06 +0530

    Added Mongoose and Cors for Network Bugs

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const cors = require("cors")
 const app = express();
 
+app.use(cors())
+app.use(express.json())
 app.get('/api/test', (req, res) => {
     res.json('test run successful')
 })
 
 app.get('/api/transaction', (req, res)=> {
+    const{ name, description, datetime} = res.body
+    
     res.json(req.body)
 })
 app.listen(4040)
+
+//smjz5j5TgQx8NAzB
